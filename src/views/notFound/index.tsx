2077b0385b783ebd05bec666a1169392f600a8e6
daniel <daniel.fonseca2002@hotmail.com>
2022-12-06 20:55:58 -0300

    adição da tela de erro 404

--- src/views/notFound/index.tsx
+++ src/views/notFound/index.tsx
@@ -1,8 +1,22 @@
-// import styles from "./not-found.module.scss";
+import { useGetTema } from "recoilState/hooks/useTema"
+import notFoundDark from "assets/notFoundDark.png"
+import styles from "./not-found.module.scss"
+import notFound from "assets/notFound.png"
+import classNames from "classnames"
 import React from "react"
 
 export default function NotFound() {
+	const mode = useGetTema()
 	return (
-		<main></main>
+		<main className={styles.container}>
+			<img src={notFound} alt="Imagem de erro 404" className={classNames({
+				[styles.image]: true,
+				[styles.disabled]: mode === "dark"
+			})} />
+			<img src={notFoundDark} alt="Imagem de erro 404" className={classNames({
+				[styles.image]: true,
+				[styles.disabled]: mode === "light"
+			})} />
+		</main>
 	)
 }
