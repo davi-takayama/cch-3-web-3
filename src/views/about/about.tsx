3f2c18420e73059b6c95a14dbef09a3df6fd17c6
daniel <daniel.fonseca2002@hotmail.com>
2022-12-08 14:46:51 -0300

    adição da página sobre

--- src/views/about/about.tsx
+++ src/views/about/about.tsx
@@ -1,9 +1,63 @@
-// import styles from "./about.module.scss"
-import React from "react"
+import { useGetTema } from "recoilState/hooks/useTema"
+import axiosInstance from "utils/axios/axios-instance"
+import React, { useEffect } from "react"
+import styles from "./about.module.scss"
+import IBrand from "utils/models/brand"
+import classNames from "classnames"
 
 export default function Home() {
+	const tema = useGetTema()
+	const api = axiosInstance
+
+	const [brands, setBrands] = React.useState<IBrand[]>([])
+	async function getBrands() {
+		const response = await api.get("/brands")
+		if (response.status === 200) setBrands(response.data)
+		else console.log("Erro ao buscar as marcas")
+	}
+	useEffect(() => {
+		getBrands()
+	}, [])
+
 	return (
-		<main>
+		<main className={classNames({
+			[styles.container]: true,
+			[styles.containerLight]: tema === "light"
+		})}>
+			<h1 className={styles.titulo}>Lista de Marcas:</h1>
+
+			<div className={styles.listagem_marcas}>
+				{
+					brands.map((marca) => (
+						<div
+							key={marca.id}
+							className={classNames({
+								[styles.listagem_marcas__item]: true,
+								[styles.listagem_marcasDark]: tema === "dark"
+							})}
+						>
+							<img
+								style={{
+									left: 0,
+									height: "20vh",
+									position: "relative",
+									margin: "0 1rem"
+								}}
+								src={marca.logo}
+								alt={marca.name}
+							/>
+							<h2>
+								{marca.description}
+							</h2>
+							<br />
+							<a href={marca.website}>
+								{marca.name}
+							</a>
+						</div>
+					))
+				}
+			</div>
+			
 		</main>
 	)
 }
